deb5c8d6aa31ed613485f3482c3d8bdd10995c5a
Maksim Istomin <mariettl@student.21-school.ru>
2025-12-25 18:07:44 +0600

    ex00-final

--- src/ex00/team01_ex00.sql
+++ src/ex00/team01_ex00.sql
@@ -3,20 +3,20 @@ WITH latest_currency AS (SELECT DISTINCT ON (id, name) id,
                                                        rate_to_usd
                          FROM currency
                          ORDER BY id, name, updated DESC),
-     balance_aggregated AS (SELECT b.user_id,
-                                   b.type,
-                                   b.currency_id,
-                                   SUM(b.money) AS volume
-                            FROM balance b
-                            GROUP BY b.user_id, b.type, b.currency_id)
-SELECT COALESCE(u.name, 'not defined')                AS name,
-       COALESCE(u.lastname, 'not defined')            AS lastname,
+     balance_aggregated AS (SELECT user_id,
+                                   type,
+                                   currency_id,
+                                   SUM(money) AS volume
+                            FROM balance
+                            GROUP BY user_id, type, currency_id)
+SELECT COALESCE(u.name, 'not defined')                 AS name,
+       COALESCE(u.lastname, 'not defined')             AS lastname,
        ba.type,
        ba.volume,
-       COALESCE(c.name, 'not defined')                AS currency_name,
-       COALESCE(c.rate_to_usd, 1)                     AS last_rate_to_usd,
-       (ba.volume * COALESCE(c.rate_to_usd, 1))::REAL AS total_volume_in_usd
+       COALESCE(lc.name, 'not defined')                AS currency_name,
+       COALESCE(lc.rate_to_usd, 1)                     AS last_rate_to_usd,
+       (ba.volume * COALESCE(lc.rate_to_usd, 1))::real AS total_volume_in_usd
 FROM balance_aggregated ba
          LEFT JOIN "user" u ON u.id = ba.user_id
-         LEFT JOIN latest_currency c ON c.id = ba.currency_id
+         LEFT JOIN latest_currency lc ON lc.id = ba.currency_id
 ORDER BY name DESC, lastname ASC, ba.type ASC;
