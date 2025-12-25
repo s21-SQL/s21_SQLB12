eda6de3b0f630d1b4d6b38c8ee598505aa4e6fca
Maksim Istomin <mariettl@student.21-school.ru>
2025-12-25 18:23:49 +0600

    ex01-final

--- src/ex01/team01_ex01.sql
+++ src/ex01/team01_ex01.sql
@@ -1,6 +1,5 @@
 INSERT INTO currency VALUES (100, 'EUR', 0.85, '2022-01-01 13:29');
 INSERT INTO currency VALUES (100, 'EUR', 0.79, '2022-01-08 13:29');
-
 WITH balance_with_nearest_rate AS (
     SELECT 
         b.user_id,
